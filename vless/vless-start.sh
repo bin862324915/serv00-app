5cb75d34346c7e1d7fb2de20740bb80763b2b4f9
wenbin <862324915@qq.com>
2024-07-07 13:17:06 +0800

    新增一键安装vless脚本

--- vless/vless-start.sh
+++ vless/vless-start.sh
@@ -5,7 +5,7 @@ wget https://raw.githubusercontent.com/bin862324915/serv00-app/main/vless/vless.
 unzip vless.zip
 
 if [ -f "vless/app.js" ]; then
-    echo "vless应用以及相关的依赖已经自动安装完成"
+    echo "vless 应用以及相关的依赖已经自动安装完成"
     echo "详细的配置步骤请参考博客文章操作，https://www.zzzwb.com/2024/07-07-serv00-vless.html"
 else
     echo "自动安装失败，请手动解压操作"
